474c96b4c18e6e425996b371a0ab9025fb50844e
Leon <yanglyu.leon.7@gmail.com>
2018-08-24 11:23:00 -0700

    demo: Implementing Controllers

---
+++ Globoma/Controllers/ConferenceController.cs
@@ -0,0 +1,38 @@
+﻿using Shared.Models;
+using Globoma.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Globoma.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        private readonly IConferenceService service;
+
+        public ConferenceController(IConferenceService service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Title = "Conference Overview";
+            return View(await service.GetAll());
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Title = "Add Conference";
+            return View(new ConferenceModel());
+        }
+
+        public async Task<IActionResult> Add(ConferenceModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await service.Add(model);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Globoma/Controllers/ProposalController.cs
@@ -0,0 +1,50 @@
+﻿using Globoma.Services;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+using System.Threading.Tasks;
+
+namespace Globoma.Controllers
+{
+    public class ProposalController : Controller
+    {
+        private readonly IConferenceService conferenceSerivce;
+        private readonly IProposalService proposalService;
+
+        public ProposalController(IConferenceService conferenceService, IProposalService proposalService)
+        {
+            this.conferenceSerivce = conferenceService;
+            this.proposalService = proposalService;
+        }
+
+        public async Task<IActionResult> Index(int conferenceId)
+        {
+            var conference = await conferenceSerivce.GetById(conferenceId);
+            ViewBag.Title = $"Proposals for Conference {conference.Name} {conference.Location}";
+            ViewBag.ConferenceId = conferenceId;
+
+            return View(await proposalService.GetAll(conferenceId));
+        }
+
+        public IActionResult Add(int conferenceId)
+        {
+            ViewBag.Title = "Add Proposal";
+            return View(new ProposalModel { ConferenceId = conferenceId });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProposalModel proposal)
+        {
+            if (ModelState.IsValid)
+            {
+                await proposalService.Add(proposal);
+            }
+            return RedirectToAction("Index", new { conferenceId = proposal.ConferenceId });
+        }
+
+        public async Task<IActionResult> Approve(int proposalId)
+        {
+            var proposal = await proposalService.Approve(proposalId);
+            return RedirectToAction("Index", new { conferenceId = proposal.ConferenceId });
+        }
+    }
+}
